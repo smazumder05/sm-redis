05e65545e3822ae2a1e6005fe3233698eb1eb284
Suvankar Mazumder <smazumder@gmail.com>
2023-05-13 22:49:49 -0400

    Protocol implementation
    Tiny redis protocol added

--- src/server.c
+++ src/server.c
@@ -10,6 +10,8 @@
 #include<netinet/ip.h>
 #include<stdbool.h>
 
+const size_t max_msg = 4096;
+
 static void err_msg(char *msg) {
     fprintf(stderr, "%s\n", msg);
 }
@@ -21,7 +23,7 @@ static void die(const char *msg) {
 }
 
 static void do_something(int connfd) {
-    char rbuf[64] = {};
+    char rbuf[64] = {0};
     ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
 
     if (n < 0) {
@@ -32,13 +34,6 @@ static void do_something(int connfd) {
 
     char wbuf[] = "world";
     write(connfd,wbuf, strlen(wbuf));
-}
-
-// Deals with multiple client requests comming from one connection
-// Will handle communication protocol
-//
-static int32_t handle_request(int connfd){
-    return 0;
 }
 
 static int32_t read_full(int fd, char *buf, size_t n){
@@ -66,6 +61,52 @@ static int32_t write_all(int fd, const char *buf,size_t n) {
     }
     return 0;
 }
+// Deals with multiple client requests comming from one connection
+// Will handle communication protocol
+//
+static int32_t handle_request(int connfd){
+    //4 bytes header
+    char rbuf[4 + max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd,rbuf,4);
+    if (err) {
+        if (errno == 0)
+            err_msg("EOF");
+        else
+            err_msg("read() error");
+        return err;
+    }
+
+    //get the message header which is 4bytes long.
+    uint32_t len = 0;
+    memcpy(&len,rbuf,4);
+    if (len > max_msg) {
+        err_msg("message too long.");
+        return -1;
+    }
+
+    //request body of the message
+    err = read_full(connfd,&rbuf[4],len);
+    if (err) {
+        err_msg("read() - failed to read message body.");
+        return err;
+    }
+
+    //do something
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n",&rbuf[4]);
+
+    //reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    //write header
+    memcpy(wbuf,&len,4);
+    //write body
+    memcpy(&wbuf[4],reply,len);
+    return write_all(connfd,wbuf,4+len);
+}
+
 /**
  * main function for the server.
  *
@@ -82,7 +123,7 @@ int main() {
     setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&val,sizeof(val));
 
     //Bind to the socket, listen and then accept connections
-    struct sockaddr_in addr = {};
+    struct sockaddr_in addr = {0};
     addr.sin_family = AF_INET;
     addr.sin_port = ntohs(1234); //The ntohs() function converts the unsigned short integer netshort from network byte order to host byte order.
     addr.sin_addr.s_addr = ntohl(0); //wildcard address 0.0.0.0
@@ -96,7 +137,7 @@ int main() {
         die("listen()");
 
     while(true) {
-        struct sockaddr_in client_addr = {};
+        struct sockaddr_in client_addr = {0};
         socklen_t socklen = sizeof(client_addr);
         int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
         if (connfd < 0)
@@ -106,8 +147,9 @@ int main() {
         //connection at a time.
         while(true){
             int32_t err = handle_request(connfd);
-            if (err)
+            if (err < 0) {
                 break;
+            }
         }
         close(connfd);
     }
